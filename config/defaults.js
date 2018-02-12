94374eb74d8e2566c9e4e391229a67d3ad22d1cb
Tudor <tudor.suditu@yahoo.com>
2018-02-12 23:37:48 +0200

    changed default port to 3007

--- config/defaults.js
+++ config/defaults.js
@@ -1,6 +1,6 @@
 const environment = process.env.NODE_ENV || 'localhost';
 module.exports = {
-    port: process.env.PORT || 3005,
+    port: process.env.PORT || 3007,
     tracingHeaderKey: process.env.TRACING_HEADER_KEY || 'x-ss-request-id',
     jwtHeaderKey: process.env.JWT_HEADER_KEY || 'x-ss-jwt',
     serviceName: 'stay-safe-data'
