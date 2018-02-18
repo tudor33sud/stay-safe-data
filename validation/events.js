6be7032d77d24fb3e5f2d7175863572b0fb2ddf0
Tudor <tudor.suditu@yahoo.com>
2018-02-18 14:09:46 +0200

    added create event validation

--- validation/events.js
+++ validation/events.js
@@ -1,11 +1,25 @@
 const { check, validationResult, body, query, header, param, allOf, oneOf } = require('express-validator/check');
-
+const _ = require('lodash');
+const helpers = require('./_helpers');
 function getEvents() {
     return [
         param('userId', 'User id should be a non empty string').isLength({ min: 1, max: 50 })
     ];
 }
+const priorities = ['low', 'medium', 'high'];
+const performerTypes = ['ambulance','doctor'];
+
+function createEvent() {
+    return [
+        body('priority', `Priorities should be one of: ${priorities.join(' ')}`).isIn(priorities),
+        body('description', 'Description should be a string between 5 and 255 characters').isLength({ min: 5, max: 255 }),
+        body('performerType', `Performer type should be one of:${performerTypes.join(' ')} `).isIn(performerTypes),
+        body('location', 'Location should be a validat latlong string').isLatLong(),
+        body('tags', 'Tags should be array of tags ids').custom(helpers.validators.isArrayOfIntegers)
+    ]
+}
 
 module.exports = {
-    getAll: getEvents
+    getAll: getEvents,
+    create: createEvent,
 }
