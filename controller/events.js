e4d5710bc6e81ed270a15cb840f2bf2750f9f231
Tudor <tudor.suditu@yahoo.com>
2018-02-18 14:09:55 +0200

    exposed create / delete events functionality

--- controller/events.js
+++ controller/events.js
@@ -22,4 +22,57 @@ router.get('/', [validation.events.getAll()], async (req, res, next) => {
     }
 });
 
+router.post('/', [validation.events.create()], async (req, res, next) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.mapped() });
+        }
+        const transaction = await db.sequelize.transaction();
+        try {
+            const { priority, description, performerType, location, tags } = req.body;
+            const foundTags = await db.tag.findAll({
+                where: { id: tags }
+            });
+            if (foundTags.length !== tags.length) {
+                throw new ApiError(`Cannot find all tags`, 404);
+            }
+            const createdEvent = await db.event.create({
+                priority,
+                description,
+                performerType,
+                location: {
+                    latlon: location
+                },
+                requester: db.event.getRequester(req)
+            }, { transaction: transaction });
+            const addEvents = foundTags.map(tag => {
+                return createdEvent.addTag(tag, { transaction: transaction });
+            });
+            await Promise.all(addEvents);
+            await transaction.commit();
+            res.status(201).json(createdEvent);
+        } catch (err) {
+            await transaction.rollback();
+            throw err;
+        }
+
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.delete('/:eventId', async (req, res, next) => {
+    try {
+        const event = await db.event.findById(req.params.eventId);
+        if (!event) {
+            throw new ApiError('Event not found', 404);
+        }
+        const deleted = await event.destroy();
+        res.status(204).json(deleted);
+    } catch (err) {
+        next(err);
+    }
+});
+
 module.exports = router;
