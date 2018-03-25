b35c069246089014d910c4448f27870b924518f3
Tudor <tudor.suditu@yahoo.com>
2018-03-25 17:37:50 +0300

    fixed events get all endpoint. added getting event by id

--- controller/events.js
+++ controller/events.js
@@ -12,11 +12,34 @@ router.get('/', [validation.events.getAll()], async (req, res, next) => {
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.mapped() });
         }
-        const events = db.event.findAll({ where: {} });
+        const events = await db.event.findAll({ where: {} });
         if (events.length === 0) {
             return res.status(204).send();
         }
-        res.json(members);
+        res.json(events);
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.get('/:eventId', async (req, res, next) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.mapped() });
+        }
+        const event = await db.event.find({
+            where: {
+                id: req.params.eventId,
+                requester: {
+                    identifier: req.referrer.id
+                }
+            }
+        });
+        if (!event) {
+            throw new ApiError(`Resource not found`, 404);
+        }
+        res.json(event);
     } catch (err) {
         next(err);
     }
--- validation/events.js
+++ validation/events.js
@@ -3,7 +3,7 @@ const _ = require('lodash');
 const helpers = require('./_helpers');
 function getEvents() {
     return [
-        param('userId', 'User id should be a non empty string').isLength({ min: 1, max: 50 })
+        //param('userId', 'User id should be a non empty string').isLength({ min: 1, max: 50 })
     ];
 }
 const priorities = ['low', 'medium', 'high'];
