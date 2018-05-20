0fb08b18d0dcf6b544b843223a42f00fc3b57aa7
Tudor <tudor.suditu@yahoo.com>
2018-05-20 03:24:07 +0300

    include tag gravity in data model

--- db/models/tag.js
+++ db/models/tag.js
@@ -5,6 +5,10 @@ module.exports = function (sequelize, DataTypes) {
             type: DataTypes.STRING,
             allowNull: false,
             unique: true
+        },
+        gravity: {
+            type: DataTypes.INTEGER,
+            allowNull: false
         }
     },
         {
